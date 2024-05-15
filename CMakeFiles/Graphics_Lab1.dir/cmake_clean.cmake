ff9a77bc068e3066e5c180fe6aca72df6857ad03
Ëemayas <prashantmanandhar2002@gmail.com>
2024-05-15 19:09:15 +0545

    up to Lab 3

---
+++ CMakeFiles/Graphics_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graphics_Lab1.dir/src/Projects/square.cpp.obj"
+  "CMakeFiles/Graphics_Lab1.dir/src/Projects/square.cpp.obj.d"
+  "Graphics_Lab1.exe"
+  "Graphics_Lab1.exe.manifest"
+  "Graphics_Lab1.pdb"
+  "libGraphics_Lab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graphics_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Graphics_Lab1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Graphics_Lab1.
+# This may be replaced when dependencies are built.
