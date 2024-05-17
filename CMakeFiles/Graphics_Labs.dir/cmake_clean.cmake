82df12bacc52817237601651f2cbc25d56d32a69
Ëemayas <prashantmanandhar2002@gmail.com>
2024-05-17 19:35:47 +0545

    Lab 4 working

---
+++ CMakeFiles/Graphics_Labs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graphics_Labs.dir/src/Lab_4/transformation.cpp.obj"
+  "CMakeFiles/Graphics_Labs.dir/src/Lab_4/transformation.cpp.obj.d"
+  "Graphics_Labs.exe"
+  "Graphics_Labs.exe.manifest"
+  "Graphics_Labs.pdb"
+  "libGraphics_Labs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graphics_Labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
